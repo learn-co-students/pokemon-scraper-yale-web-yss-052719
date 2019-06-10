33ebde09139ac78ef4859ee6696241251073aea2
leoniedong <leoniedong@hotmail.com>
2019-06-09 22:56:04 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,13 @@
+require "pry"
 class Pokemon
 
-    attr_accessor :id, :name, :type, :db
+    attr_accessor :id, :name, :type, :db, :hp
 
-    def initialize(id:, name:, type:, db:)
+    def initialize(id:, name:, type:, db:, hp:60)
         @id = id
         @name = name
         @type = type
+        @hp = hp
         @db = db
     end
 
@@ -19,5 +21,10 @@ class Pokemon
         sql = "SELECT * FROM pokemon WHERE id = ?"
         result = db.execute(sql, id)[0]
         Pokemon.new(id: result[0], name: result[1], type: result[2], db:db)
+    end
+
+    def alter_hp(num, db)
+        sql = "UPDATE pokemon SET hp = ? WHERE id = @id"
+        pokemon.execute(sql, num)
     end
 end
